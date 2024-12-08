dac3bd3d4740d6c15b1e86cd2d0195de194add9c
Emil Gothilander <emil.gothilander@gmail.com>
2024-12-08 20:42:52 +0100

    Demo version 1.0

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ from skimage.metrics import structural_similarity as ssim
 import cv2
 import pyautogui
 import datetime
+import numpy as np
 
 def main():
-    video_url = "https://www.youtube.com/watch?v=H7N2nvBdZ4g&autoplay=1"
+    # Sultans of swing
+    # video_url = "https://www.youtube.com/watch?v=H7N2nvBdZ4g&autoplay=1"
+    # Bohemian rhapsody
+    video_url = "https://www.youtube.com/watch?v=n1WomoCiYqg&autoplay=1"
     yt = YouTube(video_url)
     webbrowser.open(video_url)
     print("Playing YouTube video:")
@@ -24,6 +28,11 @@ def compare_images(image_1, image_2):
 
 def take_screenshot(x, y, width, height):
     screenshot = pyautogui.screenshot(region=(x, y, width, height))
+    
+    screenshot_cv = np.array(screenshot)
+    screenshot_cv = cv2.cvtColor(screenshot_cv, cv2.COLOR_RGB2BGR)
+    
+    return screenshot_cv
 
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     screenshot_filename = f"./images/screenshot_{timestamp}.png"
@@ -31,22 +40,33 @@ def take_screenshot(x, y, width, height):
     screenshot.save(screenshot_filename)
     print(f"Screenshot taken and saved as {screenshot_filename}")
 
+def write_img_to_file(image):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    screenshot_filename = f"./tabs/screenshot_{timestamp}.png"
+    cv2.imwrite(screenshot_filename, image)
+
 if __name__=="__main__":
     main()
     
     # Holds latest taken screenshot
     latest_screenshot = None
+    first_has_been_set = False
     
     # Image settings
     x, y = 252, 701
     width, height = 1400, 225
     
     while True:
-        if not latest_screenshot:
-            pass
+        sleep(2.5)
+        if not first_has_been_set:
             # Take screen shot for latest
+            latest_screenshot = take_screenshot(x, y, width, height)
+            first_has_been_set = True
         else:
-            pass
             # Take screen shot and compare to latest
             # If SSIM < 0.9, save latest to file and update latest_screenshot
-        sleep(1)
+            new_screenshot = take_screenshot(x, y, width, height)
+            similarity_index = compare_images(latest_screenshot, new_screenshot)
+            if similarity_index < 0.9:
+                write_img_to_file(latest_screenshot)
+            latest_screenshot = new_screenshot
