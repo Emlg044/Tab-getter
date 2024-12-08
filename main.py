cbd2b43e9227402312cf43fe324bbe8d2dd80e83
Emil Gothilander <emil.gothilander@gmail.com>
2024-12-08 18:28:21 +0100

    Commented out code

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from time import sleep
 import webbrowser
 from skimage.metrics import structural_similarity as ssim
 import cv2
+import pyautogui
+import datetime
 
 def main():
     video_url = "https://www.youtube.com/watch?v=H7N2nvBdZ4g&autoplay=1"
@@ -10,15 +12,24 @@ def main():
     webbrowser.open(video_url)
     print("Playing YouTube video:")
 
-image1 = cv2.imread("./images/screenshot_2024-12-08_18-06-02.png")
-image2 = cv2.imread("./images/screenshot_2024-12-08_18-06-04.png")
+# image1 = cv2.imread("./images/screenshot_2024-12-08_18-06-02.png")
+# image2 = cv2.imread("./images/screenshot_2024-12-08_18-06-04.png")
 
 def compare_images(image_1, image_2):
-    gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
-    gray2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+    gray1 = cv2.cvtColor(image_1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(image_2, cv2.COLOR_BGR2GRAY)
 
     similarity_index, _ = ssim(gray1, gray2, full=True)
     return similarity_index
+
+def take_screenshot(x, y, width, height):
+    screenshot = pyautogui.screenshot(region=(x, y, width, height))
+
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    screenshot_filename = f"./images/screenshot_{timestamp}.png"
+    
+    screenshot.save(screenshot_filename)
+    print(f"Screenshot taken and saved as {screenshot_filename}")
 
 if __name__=="__main__":
     main()
